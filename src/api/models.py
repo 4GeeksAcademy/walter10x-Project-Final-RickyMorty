89ef821e839610a39dfeb2a461bca3b38860fc32
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-07-12 14:04:00 +0000

    create models user and favoriteCharacter

--- src/api/models.py
+++ src/api/models.py
@@ -5,15 +5,25 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(50), nullable=True)
+    last_name = db.Column(db.String(50), nullable=True)
+    favorites = db.Column(db.Integer, default=0)  # Ejemplo de campo favoritos
+    favorite_characters = db.relationship('FavoriteCharacter', back_populates='user', cascade="all, delete-orphan")
 
     def __repr__(self):
         return f'<User {self.email}>'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
+
+class FavoriteCharacter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    character_id = db.Column(db.Integer, nullable=False)
+    user = db.relationship('User', back_populates='favorite_characters')
+
+    def __repr__(self):
+        return f'<FavoriteCharacter user_id={self.user_id} character_id={self.character_id}>'
+
+
+
